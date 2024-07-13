3fd900322002892d93c82b25118dc7077f2d4085
Reques6e <voronin99borja61275y@rambler.ru>
2024-07-13 13:46:55 +0300

    Обновил конфиг под DataBase

--- config.py
+++ config.py
@@ -4,3 +4,12 @@ class Config:
     cdn_domain = ''
 
     amount_limit = 25000
+
+    class DataBase:
+        db_config = {
+            'host': '127.0.0.1',
+            'port': 3306,
+            'user': '',
+            'password': '',
+            'db': ''
+        }
