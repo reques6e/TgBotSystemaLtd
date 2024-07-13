696b3f049510cfcb93eddb091e47c48b2fa50bf1
Reques6e <voronin99borja61275y@rambler.ru>
2024-07-13 13:46:43 +0300

    Полностью переделал db из sqlite3 в mysql

--- bot/services/db.py
+++ bot/services/db.py
@@ -1,113 +1,105 @@
-import aiosqlite
+import aiomysql
+
+from config import Config
 
 class DataBase:
     def __init__(self) -> None:
-        self.db_path = 'db.db'
+        self.db_config = Config.DataBase.db_config
 
     async def table_create(self):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute('''CREATE TABLE IF NOT EXISTS users(
-                    user_id INTEGER PRIMARY KEY,
-                    token TEXT,
-                    id INTEGER,
-                    password TEXT,
-                    is_admin INTEGER 
-                )
-            ''')
+        pool = await aiomysql.create_pool(**self.db_config)
 
-            await db.execute('''CREATE TABLE IF NOT EXISTS favorites(
-                    user_id INTEGER PRIMARY KEY,
-                    cams TEXT
-                )
-            ''')
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS users(
+                        user_id BIGINT PRIMARY KEY,
+                        token TEXT,
+                        id BIGINT,
+                        password TEXT,
+                        is_admin INTEGER
+                    )
+                ''')
+                await conn.commit()
 
-            await db.commit()
+                await cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS favorites(
+                        user_id BIGINT PRIMARY KEY,
+                        cams TEXT
+                    )
+                ''')
+                await conn.commit()
 
-        return
-    
-    async def get_user_info(
-        self,
-        user_id: int
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)) as cursor:
-                return await cursor.fetchone()
-        
-    async def delete_user_data(
-        self,
-        user_id: int
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
-            await db.execute("DELETE FROM favorites WHERE user_id = ?", (user_id,))
+        pool.close()
+        await pool.wait_closed()
 
-            await db.commit()
+    async def get_user_info(self, user_id: int):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+                    return await cursor.fetchone()
 
-    async def update_token(
-        self,
-        user_id: int,
-        token: str
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("UPDATE users SET token = ? WHERE user_id = ?", (token, user_id))
+    async def delete_user_data(self, user_id: int):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
+                    await cursor.execute("DELETE FROM favorites WHERE user_id = %s", (user_id,))
+                    await conn.commit()
 
-            await db.commit()
+    async def update_token(self, user_id: int, token: str):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("UPDATE users SET token = %s WHERE user_id = %s", (token, user_id))
+                    await conn.commit()
 
-    async def update_password(
-        self,
-        user_id: int,
-        password: str
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("UPDATE users SET password = ? WHERE user_id = ?", (password, user_id))
+    async def update_password(self, user_id: int, password: str):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("UPDATE users SET password = %s WHERE user_id = %s", (password, user_id))
+                    await conn.commit()
 
-            await db.commit()
+    async def update_admin(self, user_id: int, admin: int):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("UPDATE users SET is_admin = %s WHERE user_id = %s", (admin, user_id))
+                    await conn.commit()
 
-    async def update_admin(
-        self,
-        user_id: int,
-        admin: int
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("UPDATE users SET is_admin = ? WHERE user_id = ?", (admin, user_id))
+    async def add_user(self, user_id: int, token: str, id: int, password: str, is_admin: int):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute(
+                        'INSERT INTO users (user_id, token, id, password, is_admin) VALUES (%s, %s, %s, %s, %s)', 
+                        (user_id, token, id, password, is_admin)
+                    )
+                    await cursor.execute(
+                        'INSERT INTO favorites (user_id, cams) VALUES (%s, %s)', 
+                        (user_id, '[]')
+                    )
+                    await conn.commit()
 
-            await db.commit()
+    async def get_cams(self, user_id: int):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("SELECT cams FROM favorites WHERE user_id = %s", (user_id,))
+                    return await cursor.fetchone()
 
-    async def add_user(
-        self,
-        user_id: int,
-        token: str,
-        id: int,
-        password: str,
-        is_admin: int
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute('INSERT INTO users (user_id, token, id, password, is_admin) VALUES (?, ?, ?, ?, ?)', (user_id, token, id, password, is_admin))
-            await db.execute('INSERT INTO favorites (user_id, cams) VALUES (?, ?)', (user_id, '[]'))
-            
-            await db.commit()
+    async def cam_update(self, user_id: int, dump: dict):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("REPLACE INTO favorites (user_id, cams) VALUES (%s, %s)", (user_id, dump))
+                    await conn.commit()
 
-    async def get_cams(
-        self,
-        user_id: int
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT cams FROM favorites WHERE user_id = ?", (user_id,)) as cursor:
-                return await cursor.fetchone()
-        
-    async def cam_update(
-        self,
-        user_id: int,
-        dump: dict
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("REPLACE INTO favorites (user_id, cams) VALUES (?, ?)", (user_id, dump))
-
-            await db.commit()
-
-    async def get_all_user_id(
-        self
-    ):
-        async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT user_id FROM users") as cursor:
-                return await cursor.fetchall()
+    async def get_all_user_id(self):
+        async with aiomysql.create_pool(**self.db_config) as pool:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.execute("SELECT user_id FROM users")
+                    return await cursor.fetchall()
